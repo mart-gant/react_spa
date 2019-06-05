b01a36cd153fb60626390ec4a475db1bec6dfd92
User <martgant@gmail.com>
2019-06-05 13:50:09 +0200

    Final commit

--- src/Contact.js
+++ src/Contact.js
@@ -4,9 +4,8 @@ class Contact extends Component {
     render() {
         return (
             <div>
-                <h2>GOT QUESTIONS?</h2>
-                <p>The easiest thing to do is post on
-                    our <a href="http://forum.kirupa.com">forums</a>.
+                <h2>Any questions</h2>
+                <p>The answer is<a href="https://stackoverflow.com/">forums</a>.
                 </p>
             </div>
         );
